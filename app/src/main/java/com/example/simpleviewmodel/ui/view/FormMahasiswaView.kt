37f144e147669834189a3b01d9fd4d5af684a666
moh. nurwahid <mohnurwaid923@gmail.com>
2024-11-21 11:29:08 +0700

    memperbaiki FormMahasiswaView

--- app/src/main/java/com/example/simpleviewmodel/ui/view/FormMahasiswaView.kt
+++ app/src/main/java/com/example/simpleviewmodel/ui/view/FormMahasiswaView.kt
@@ -1,16 +1,12 @@
 package com.example.simpleviewmodel.ui.view
 
-import android.widget.Button
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,124 +17,87 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.w3c.dom.Text
 
 @Composable
 fun FormMahasiswaView(
     modifier: Modifier = Modifier,
     listGender: List<String>,
-    onSubmitClick: (MutableList<String>)-> Unit){
+    onSubmitClick: (MutableList<String>) -> Unit
+) {
     var nama by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
-    var noTlpn by remember { mutableStateOf("") }
+    var noTelpon by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
-    var memilihjk by remember { mutableStateOf("") }
+    var memilihJK by remember { mutableStateOf("") }
 
-    val listData: MutableList<String> = mutableListOf(
-        nama, memilihjk, alamat
+    val listData : MutableList<String> = mutableListOf(
+        nama, memilihJK, alamat, email, noTelpon
     )
 
-    Column (modifier = modifier
+    Column (modifier = Modifier
         .fillMaxSize()
         .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment =  Alignment.CenterHorizontally
     ){
         Text("Biodata",
-            fontSize = 20.sp,
+            fontSize = 25.sp,
             fontWeight = FontWeight.Bold)
-        Spacer(Modifier .padding(20.dp))
+
+        Spacer(Modifier.padding(16.dp))
+
         TextField(
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
+            modifier = Modifier .fillMaxWidth() .padding(5.dp),
             value = nama,
             onValueChange = {nama = it},
-            label = { Text("Nama")},
-            placeholder = { Text("Masukan nama")}
+            label = { Text("nama")},
+            placeholder = { Text("Masukkan Nama Anda")}
         )
 
         Row {
             listGender.forEach { item ->
-                Row (verticalAlignment = Alignment.CenterVertically) {
-                    RadioButton(
-                        selected = memilihjk == item,
-                        onClick = {
-                            memilihjk = item
-                        }
-                    )
+                Row(verticalAlignment = Alignment.CenterVertically ) { RadioButton(
+                    selected = memilihJK == item,
+                    onClick = {
+                        memilihJK = item
+                    }
+                )
                     Text(item)
+
                 }
             }
         }
 
         TextField(
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
+            modifier = Modifier .fillMaxWidth() .padding(5.dp),
             value = email,
             onValueChange = {email = it},
-            label = { Text("Email")},
-            placeholder = { Text("Masukan Email Anda")}
+            label = { Text("email")},
+            placeholder = { Text("Masukkan email Anda")}
         )
+
         TextField(
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
-            value = noTlpn,
-            onValueChange = {noTlpn = it},
-            label = { Text("NoHandphone")},
-            placeholder = { Text("Masukan No Handphone")}
+            modifier = Modifier .fillMaxWidth() .padding(5.dp),
+            value = noTelpon,
+            onValueChange = {noTelpon = it},
+            label = { Text("NoTelpon")},
+            placeholder = { Text("Masukkan NoTelpon Anda")}
         )
+
         TextField(
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
+            modifier = Modifier .fillMaxWidth() .padding(5.dp),
             value = alamat,
             onValueChange = {alamat = it},
-            label = { Text("Alamat")},
-            placeholder = { Text("Masukan Alamat Anda")}
+            label = { Text("alamat")},
+            placeholder = { Text("Masukkan alamat Anda")}
         )
 
         Button (onClick = {
             onSubmitClick(listData)
         }) {
-            Text("Simpan")
+            Text("Submit")
         }
-
-        ElevatedCard(
-            colors = CardDefaults.cardColors(containerColor = Color.LightGray),
-            modifier = modifier
-                .fillMaxWidth(),
-
-            ) {
-
-        }
-
-    }
-}
-
-@Composable
-fun DetailMessage(
-    judul: String, isinya: String
-){
-    Row (
-        modifier = Modifier.fillMaxWidth()
-            .padding(10.dp)
-    ){
-        Text(text = judul,
-            modifier = Modifier.weight(0.8f))
-        Text(text = ":",
-            modifier = Modifier.weight(0.2f))
-        Text(text = isinya,
-            modifier = Modifier.weight(2f))
     }
 }
