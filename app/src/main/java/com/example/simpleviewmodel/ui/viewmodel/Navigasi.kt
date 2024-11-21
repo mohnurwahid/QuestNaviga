0b024a2aa3664b840457d442aa34d8fb22832466
moh. nurwahid <mohnurwaid923@gmail.com>
2024-11-21 11:43:57 +0700

    membuat fun Navigasi

--- app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
@@ -1,6 +1,11 @@
 package com.example.simpleviewmodel.ui.viewmodel
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.internal.composableLambda
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import java.lang.reflect.Modifier
@@ -16,3 +21,24 @@ fun Navigasi(
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
 ) {
+
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Form.name
+        ) {
+            composable(route = Halaman.Form.name) {
+                val konteks = LocalContext.current
+                FormMahasiswaView(
+
+                    //Di bawah ini merupakan dari parameter halaman FormulirView
+                    listGender = ListGender.listGender.map { id -> konteks.getString(id)
+                    },
+                    onSubmitClick = {
+                        viewModel.savedDataMhs(it)
+                        navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
+}
